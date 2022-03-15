432f80f6769b861fd5359dbad36a7054dd80321d
Krzysztof P <qerbitz@gmail.com>
2022-03-15 18:47:49 +0100

    final_v1_front

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080'
-  //apiUrl: 'https://spring-angular-backend-shop.herokuapp.com'
+  //apiUrl: 'http://localhost:8080'
+  apiUrl: 'https://spring-angular-backend-shop.herokuapp.com'
 };
