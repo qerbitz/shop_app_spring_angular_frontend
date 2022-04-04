8db89c856fe4b2d2e2534715bba146987703b503
Krzysztof P <qerbitz@gmail.com>
2022-04-04 16:42:23 +0200

    Changed templates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8081'
+ // apiUrl: 'http://localhost:8081'
+  apiUrl: 'https://spring-angular-backend-shop.herokuapp.com'
+  
 };
 
 /*
